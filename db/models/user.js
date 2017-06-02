4bc8f0d932c55101b2944335f61c2962f6ced4a8
Ketevann <ketevan.tsin@gmail.com>
2017-06-02 16:54:54 -0400

    removed console logs

--- db/models/user.js
+++ db/models/user.js
@@ -6,11 +6,14 @@ var db = require('../index.js')
 var User = db.define('user', {
   name: {
     type: Sequelize.STRING,
-
+    allowNull: false
   },
   email: {
     type: Sequelize.STRING,
-
+    unique: true,
+    validate: {
+      isEmail: true,
+    }
   }
 });
 
